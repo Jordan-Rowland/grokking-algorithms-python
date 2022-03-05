b27553fe80dc5d384ba21b45e36a292d59bbf29a
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2022-03-05 15:01:30 -0800

    updates

--- chap9.py
+++ chap9.py
@@ -2,7 +2,7 @@
 
 
 # Find longest common substring between word_a and word_b
-def longest_common_substring(word_a, word_b):
+def longest_common_substring(word_a, word_b, subsequence=False):
     word_a = [*word_a]
     word_b = [*word_b]
     grid = [[0] * len(word_a) for _ in word_b]
@@ -11,27 +11,14 @@ def longest_common_substring(word_a, word_b):
             if word_b[y_idx] == word_a[x_idx]:
                 row[x_idx] = 1
                 row[x_idx] += grid[y_idx-1][x_idx-1]
-    return grid
-
-
-# Find longest common subsequence between word_a and word_b
-def longest_common_subsequence(word_a, word_b):
-    word_a = [*word_a]
-    word_b = [*word_b]
-    grid = [[0] * len(word_a) for _ in word_b]
-    for y_idx, row in enumerate(grid):
-        for x_idx, _ in enumerate(row):
-            if word_b[y_idx] == word_a[x_idx]:
-                row[x_idx] = 1
-                row[x_idx] += grid[y_idx-1][x_idx-1]
-            else:
+            elif subsequence:
                 row[x_idx] = max(grid[y_idx-1][x_idx], grid[y_idx][x_idx-1])
     return grid
 
 
-for row in longest_common_substring("FISH", "FOSH"):
+for row in longest_common_substring("FISH", "FOSH", subsequence=False):
     print(row)
 
 
-for row in longest_common_subsequence("FISH", "FOSH"):
+for row in longest_common_substring("FISH", "FOSH", subsequence=True):
     print(row)
