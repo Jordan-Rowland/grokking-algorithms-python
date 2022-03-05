9666d379f7788c374efe346a3a7357f82dc49bda
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2022-03-05 13:30:30 -0800

    finish cahp9

--- chap9.py
+++ chap9.py
@@ -1,21 +1,37 @@
 # DYNAMIC PROGRAMMING
 
 
-# Given word_a, find the matching letters between word_b
-word_a = [*"FOSH"]
-word_b = [*"FISH"]
-grid = [[0] * len(word_a) for _ in word_b]
-
-for y_idx, row in enumerate(grid):
-    for x_idx, _ in enumerate(row):
-        # print()
-        print(x_idx)
-        # print(f"{(x_idx, y_idx), word_b[x_idx]=}")
-        # print(f"{(x_idx, y_idx), word_a[x_idx]=}")
-        if word_b[y_idx] == word_a[x_idx]:
-            row[x_idx] = 1
-            # row[x_idx] += grid[y_idx-1][x_idx-1]
+# Find longest common substring between word_a and word_b
+def longest_common_substring(word_a, word_b):
+    word_a = [*word_a]
+    word_b = [*word_b]
+    grid = [[0] * len(word_a) for _ in word_b]
+    for y_idx, row in enumerate(grid):
+        for x_idx, _ in enumerate(row):
+            if word_b[y_idx] == word_a[x_idx]:
+                row[x_idx] = 1
+                row[x_idx] += grid[y_idx-1][x_idx-1]
+    return grid
 
 
-for row in grid:
+# Find longest common subsequence between word_a and word_b
+def longest_common_subsequence(word_a, word_b):
+    word_a = [*word_a]
+    word_b = [*word_b]
+    grid = [[0] * len(word_a) for _ in word_b]
+    for y_idx, row in enumerate(grid):
+        for x_idx, _ in enumerate(row):
+            if word_b[y_idx] == word_a[x_idx]:
+                row[x_idx] = 1
+                row[x_idx] += grid[y_idx-1][x_idx-1]
+            else:
+                row[x_idx] = max(grid[y_idx-1][x_idx], grid[y_idx][x_idx-1])
+    return grid
+
+
+for row in longest_common_substring("FISH", "FOSH"):
+    print(row)
+
+
+for row in longest_common_subsequence("FISH", "FOSH"):
     print(row)
